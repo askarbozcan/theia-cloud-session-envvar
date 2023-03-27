ab77a095f22bc503fb6609e329f98ce2cd23aebd
askarbozcan <askar.bozcan@metu.edu.tr>
2023-03-27 16:20:24 +0300

    refactor deployment addition tests

--- java/operator/org.eclipse.theia.cloud.operator/src/test/java/JavaResourceUtilTest.java
+++ java/operator/org.eclipse.theia.cloud.operator/src/test/java/JavaResourceUtilTest.java
@@ -1,12 +1,15 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -15,22 +18,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.eclipse.theia.cloud.operator.util.JavaResourceUtil;
 
 public class JavaResourceUtilTest {
-    static String resourcesPath = Paths.get("./", "src", "test", "resources").toString();
+    final static String resourcesPath = Paths.get("./", "src", "test", "resources").toString();
+
+    static String testYaml;
+    static String expectedYaml;
+
+    @BeforeAll
+    static void loadYamls() throws IOException {
+        String testYamlPath = Paths.get(resourcesPath, "test_templates", "templateDeployment_bare_envvar.yaml").toString();
+        String expectedYamlPath = Paths.get(resourcesPath, "test_templates", "templateDeployment_bare_envvar_ground_truth.yaml").toString();
+
+        testYaml = new String(Files.readAllBytes(Paths.get(testYamlPath)));
+        expectedYaml = new String(Files.readAllBytes(Paths.get(expectedYamlPath)));
+    }
 
     // TODO: Add tests for malformed yaml, non existing container
     // TODO: ownerReference and namespace fields added to test yaml?!!! WTF? Where does it add.
     @Test
-    void testHappyPath_addEnvVarMapToDeploymentYaml() throws FileNotFoundException, IOException {
+    void testHappyPath_addEnvVarMapToDeploymentYaml() throws JsonProcessingException{
     
-        String testYamlPath = Paths.get(resourcesPath, "test_templates", "templateDeployment_bare_envvar.yaml").toString();
-        String expectedYamlPath = Paths.get(resourcesPath, "test_templates", "templateDeployment_bare_envvar_ground_truth.yaml").toString();
-
-        String testYaml = new String(Files.readAllBytes(Paths.get(testYamlPath)));
-        String expectedYaml = new String(Files.readAllBytes(Paths.get(expectedYamlPath)));
         Map<String, String> newEnvVars = Map.of(
             "UNICORN_COLOR", "orange",
             "HIPPO_WEIGHT", "89kg"
@@ -44,6 +55,19 @@ public class JavaResourceUtilTest {
 
         assertEquals(expectedTree, producedTree);
     }
+
+    @Test
+    void testNonExistingContainer_addEnvVarMapToDeploymentYaml() {
+        Map<String, String> newEnvVars = Map.of(
+            "UNICORN_COLOR", "orange",
+            "HIPPO_WEIGHT", "89kg"
+        );
+
+        assertThrows(NoSuchElementException.class, () -> {
+            JavaResourceUtil.addEnvVarMapToDeploymentYaml(testYaml, newEnvVars, "not-existing-container");
+        });
+    }
+
 
     
 }
